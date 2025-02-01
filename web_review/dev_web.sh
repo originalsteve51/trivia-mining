075682006b71cd738ee93ae85405fb3ff80ae186
Stephen Harding <ssh751@gmail.com>
2025-02-01 12:11:32 -0500

    major restructure update

--- web_review/dev_web.sh
+++ web_review/dev_web.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 export RUN_ON_HOST="localhost"
-export USING_PORT=8081
+export USING_PORT=8082
 export UPDATE_INTERVAL=1
 export DEBUG_MODE=True
 
-python -i setlist_web.py
+python web_review.py
